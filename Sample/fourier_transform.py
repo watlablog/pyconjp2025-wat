f55aed211ff5701f7ab2c6b890c6cba322183db6
wat <72868821+watlablog@users.noreply.github.com>
2025-09-06 22:22:11 +0900

    Add fourier_transform.py

---
+++ Sample/fourier_transform.py
@@ -0,0 +1,68 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import soundfile as sf
+
+
+def fourier_transform(t, x):
+    """フーリエ変換をする関数"""
+    
+    N  = len(x)
+    dt = t[1] - t[0]
+
+    # 実数信号用の片側FFTと周波数軸
+    X = np.fft.rfft(x)
+    f = np.fft.rfftfreq(N, d=dt)
+
+    # 振幅スペクトル
+    amp = np.abs(X) / N
+    if len(amp) > 2:
+        amp[1:-1] *= 2
+        
+    return f, amp
+
+
+def plot(t, x, freq, amp):
+    """波形を確認する関数"""
+    
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
+
+    fig = plt.figure(figsize=(6, 8))
+    ax1 = fig.add_subplot(211); ax2 = fig.add_subplot(212)
+    for ax in (ax1, ax2):
+        ax.yaxis.set_ticks_position('both')
+        ax.xaxis.set_ticks_position('both')
+
+    ax1.set_xlabel('Time[s]')
+    ax1.set_ylabel('Amplitude')
+    ax2.set_xlabel('Frequency[Hz]')
+    ax2.set_ylabel('Amplitude')
+    ax2.set_xlim(0.0, 2000.0)
+    #ax2.set_ylim(1e-6, 1e-1)
+    ax2.set_yscale('log')
+
+    ax1.plot(t, x, label='time', lw=1, color='red')
+    ax2.plot(freq, amp, label='freq', lw=1, color='red')
+
+    fig.tight_layout()
+    plt.show()
+    plt.close()
+    
+    return
+
+if __name__ == '__main__':
+    """メイン"""
+    
+    # wav波形の読み込み
+    path = 'wav/recorded.wav'
+    data, samplerate = sf.read(path)
+    
+    # 時間軸の作成
+    dt = 1 / samplerate
+    t = np.arange(0, len(data)*dt, dt)
+    
+    # フーリエ変換とグラフ表示
+    freq, amp = fourier_transform(t, data)
+    plot(t, data, freq, amp)
